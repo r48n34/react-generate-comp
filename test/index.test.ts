0ef15d611eae7418766e8b557cf4eb0172b4980e
reemo <r48n34@gmail.com>
2022-09-16 07:10:36 +0800

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import { exec } from "child_process"
 import fs from "fs"
 
+// test generate comp
 test("Command 'rgc -tc helloComp' works", (done:any) => {
     exec('rgc -tc helloComp', (err, out) => {
         const listDir = fs.readdirSync("./");
@@ -17,6 +18,38 @@ test("Command 'rgc -t -c yoloComp' works", (done:any) => {
         expect(listDir).toContain('YoloComp.tsx');
 
         fs.unlinkSync("./YoloComp.tsx");
+        done();
+    });
+});
+
+test("Command 'rgc -c orangeComp' works", (done:any) => {
+    exec('rgc -c orangeComp', (err, out) => {
+        const listDir = fs.readdirSync("./");
+        expect(listDir).toContain('OrangeComp.jsx');
+
+        fs.unlinkSync("./OrangeComp.jsx");
+        done();
+    });
+});
+
+
+// test generate slice
+test("Command 'rgc -ts apple' works", (done:any) => {
+    exec('rgc -ts apple', (err, out) => {
+        const listDir = fs.readdirSync("./");
+        expect(listDir).toContain('AppleSlice.tsx');
+
+        fs.unlinkSync("./AppleSlice.tsx");
+        done();
+    });
+});
+
+test("Command 'rgc -s apple' works", (done:any) => {
+    exec('rgc -s apple', (err, out) => {
+        const listDir = fs.readdirSync("./");
+        expect(listDir).toContain('AppleSlice.jsx');
+
+        fs.unlinkSync("./AppleSlice.jsx");
         done();
     });
 });
