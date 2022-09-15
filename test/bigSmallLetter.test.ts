338452550e50f150770ce2fcca70129edb400415
reemo <r48n34@gmail.com>
2022-09-16 07:23:13 +0800

    add more test cases

--- test/bigSmallLetter.test.ts
+++ test/bigSmallLetter.test.ts
@@ -1,11 +1,15 @@
 import { strToSmallAndBig } from "../src/utilis/supportGen/bigSmallLetter"
 
-test('strToSmallAndBig function works in normal', () => {
-    const testString = "helloComp";
-    expect(strToSmallAndBig(testString)).toEqual(["HelloComp", "helloComp"]);
-});
+describe('test strToSmallAndBig functions', () => {
 
-test('strToSmallAndBig function works in blank string', () => {
-    const testString = "";
-    expect(strToSmallAndBig(testString)).toEqual(["Values", "values"]);
+    test('strToSmallAndBig function works in normal', () => {
+        const testString = "helloComp";
+        expect(strToSmallAndBig(testString)).toEqual(["HelloComp", "helloComp"]);
+    });
+
+    test('strToSmallAndBig function works in blank string', () => {
+        const testString = "";
+        expect(strToSmallAndBig(testString)).toEqual(["Values", "values"]);
+    });
+
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,55 +1,98 @@
 import { exec } from "child_process"
 import fs from "fs"
 
-// test generate comp
-test("Command 'rgc -tc helloComp' works", (done:any) => {
-    exec('rgc -tc helloComp', (err, out) => {
-        const listDir = fs.readdirSync("./");
-        expect(listDir).toContain('HelloComp.tsx');
-
-        fs.unlinkSync("./HelloComp.tsx");
-        done();
+describe('Test generate comp', () => {
+  
+    test("Command 'rgc -tc helloComp' works", (done:any) => {
+        exec('rgc -tc helloComp', (err, out) => {
+            const listDir = fs.readdirSync("./");
+            expect(listDir).toContain('HelloComp.tsx');
+    
+            fs.unlinkSync("./HelloComp.tsx");
+            done();
+        });
     });
+    
+    test("Command 'rgc -t -c yoloComp' works", (done:any) => {
+        exec('rgc -tc yoloComp', (err, out) => {
+            const listDir = fs.readdirSync("./");
+            expect(listDir).toContain('YoloComp.tsx');
+    
+            fs.unlinkSync("./YoloComp.tsx");
+            done();
+        });
+    });
+    
+    test("Command 'rgc -c orangeComp' works", (done:any) => {
+        exec('rgc -c orangeComp', (err, out) => {
+            const listDir = fs.readdirSync("./");
+            expect(listDir).toContain('OrangeComp.jsx');
+    
+            fs.unlinkSync("./OrangeComp.jsx");
+            done();
+        });
+    });
+    
+    test("Command 'rgc -ts' remain no changes", (done:any) => {
+    
+        const listDirBefore = fs.readdirSync("./");
+    
+        exec('rgc -ts', (err, out) => {
+            const listDirAfter = fs.readdirSync("./");
+
+            expect(listDirAfter.length).toBe(listDirBefore.length);
+    
+            done();
+        });
+    });
+
 });
 
-test("Command 'rgc -t -c yoloComp' works", (done:any) => {
-    exec('rgc -tc yoloComp', (err, out) => {
-        const listDir = fs.readdirSync("./");
-        expect(listDir).toContain('YoloComp.tsx');
+describe('Test generate slice', () => {
 
-        fs.unlinkSync("./YoloComp.tsx");
-        done();
+    test("Command 'rgc -ts apple' works", (done:any) => {
+        exec('rgc -ts apple', (err, out) => {
+            const listDir = fs.readdirSync("./");
+            expect(listDir).toContain('AppleSlice.tsx');
+    
+            fs.unlinkSync("./AppleSlice.tsx");
+            done();
+        });
     });
+    
+    test("Command 'rgc -s apple' works", (done:any) => {
+        exec('rgc -s apple', (err, out) => {
+            const listDir = fs.readdirSync("./");
+            expect(listDir).toContain('AppleSlice.jsx');
+    
+            fs.unlinkSync("./AppleSlice.jsx");
+            done();
+        });
+    });
+
 });
 
-test("Command 'rgc -c orangeComp' works", (done:any) => {
-    exec('rgc -c orangeComp', (err, out) => {
-        const listDir = fs.readdirSync("./");
-        expect(listDir).toContain('OrangeComp.jsx');
+describe('Test Remaining cases.', () => {
+    test("Command 'rgc -t' remain no changes", (done:any) => {
+        
+        const listDirBefore = fs.readdirSync("./");
 
-        fs.unlinkSync("./OrangeComp.jsx");
-        done();
+        exec('rgc -t', (err, out) => {
+            const listDirAfter = fs.readdirSync("./");
+            expect(listDirAfter.length).toBe(listDirBefore.length);
+            done();
+        });
     });
-});
 
+    test("Command 'rgc' remain no changes", (done:any) => {
+        
+        const listDirBefore = fs.readdirSync("./");
 
-// test generate slice
-test("Command 'rgc -ts apple' works", (done:any) => {
-    exec('rgc -ts apple', (err, out) => {
-        const listDir = fs.readdirSync("./");
-        expect(listDir).toContain('AppleSlice.tsx');
-
-        fs.unlinkSync("./AppleSlice.tsx");
-        done();
+        exec('rgc', (err, out) => {
+            const listDirAfter = fs.readdirSync("./");
+            expect(listDirAfter.length).toBe(listDirBefore.length);
+            done();
+        });
     });
-});
 
-test("Command 'rgc -s apple' works", (done:any) => {
-    exec('rgc -s apple', (err, out) => {
-        const listDir = fs.readdirSync("./");
-        expect(listDir).toContain('AppleSlice.jsx');
-
-        fs.unlinkSync("./AppleSlice.jsx");
-        done();
-    });
 });
